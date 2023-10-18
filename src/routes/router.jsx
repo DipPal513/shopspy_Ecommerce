e1024b01f5b544d16d42d8161784352748c56690
dippal513 <dip.pal.513@gmail.com>
2023-10-18 07:04:17 +0600

    add: deploy workflow

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,30 +1,29 @@
 import * as React from "react";
-import { createBrowserRouter } from "react-router-dom";
-
+import { createBrowserRouter, Route, Outlet } from "react-router-dom";
+import Header from "../Components/Header";
 import Home from "../pages/Home";
-
+import Products from "../pages/Product/Products";
+import NotFound from "../pages/NotFound";
+import DisplayProduct from "../pages/Product/displayProduct";
+import CartItems from "../pages/CartItems";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element: <Home />
-  }
-  // {
-  //   path: "",
-  //   element: (
-  //     <>
-  //       <Header />
-  //       <Outlet /> {/* This renders the nested routes */}
-  //     </>
-  //   ),
-  //   children: [
-  //     { path: "", element: <Home /> },
-  //     { path: "products", element: <Products /> },
-  //     { path: "product/:id", element: <DisplayProduct /> },
-  //     { path: "cartitems", element: <CartItems /> }, // Use CartItems instead of CartItem
-  //     { path: "*", element: <NotFound /> },
-  //   ],
-  // },
+    path: "",
+    element: (
+      <>
+        <Header />
+        <Outlet /> {/* This renders the nested routes */}
+      </>
+    ),
+    children: [
+      { path: "", element: <Home /> },
+      { path: "products", element: <Products /> },
+      { path: "product/:id", element: <DisplayProduct /> },
+      { path: "cartitems", element: <CartItems /> }, // Use CartItems instead of CartItem
+      { path: "*", element: <NotFound /> },
+    ],
+  },
 ]);
 
 export default router;
