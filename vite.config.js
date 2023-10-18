05b13af792ab0a2bc674fea0e3e4992bbf37bdee
dippal513 <dip.pal.513@gmail.com>
2023-10-18 07:11:35 +0600

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[shopspy_ecommerce]/",
-  build: {
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom", "react-redux"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  }
+  base: "/shopspy_ecommerce/",
+ 
 })
