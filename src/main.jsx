7e86f366954f495801db9b5ccfcecc77534c1c03
dippal513 <dip.pal.513@gmail.com>
2023-10-18 06:41:47 +0600

    add: deploy workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Router.jsx'
-import Header from './Components/Header.jsx'
-import ShopContextProvider from './context/ShopContext'
-import ScrollToTop from 'react-scroll-to-top'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Router.jsx";
+import ShopContextProvider from "./context/ShopContext";
+import ScrollToTop from "react-scroll-to-top";
 // import ShopContextProvider from './context/ShopContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ShopContextProvider>
-    {/* <Header /> */}
     <RouterProvider router={router} />
-    {
-      window.scrollY > 100 &&
-    <ScrollToTop smooth />
-    }
-
+    {window.scrollY > 100 && <ScrollToTop smooth />}
   </ShopContextProvider>
 );
